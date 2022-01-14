3c387095be8ec5b7cc473c1ff5fa5cb093fb5c55
biggaji <herityjohnny14@gmail.com>
2022-01-14 17:51:24 +0100

    Created cwLogger class and configured it to use aws service based credentials

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,34 @@
 import aws from 'aws-sdk';
+import { logs, getLogNSTParams, sendLogsParams, awsCred } from '../types/loggerTypes';
 
-const cwl = new aws.CloudWatchLogs({});
+const cwl = new aws.CloudWatchLogs({ apiVersion: '2014-03-28' });
 
+/**
+ * @class cwLogger
+ * @description cwLogger class
+ * @author Tobi Ajibade
+ */
+
+export class cwLogger {
+    protected aws_cred: awsCred;
+    
+    constructor(aws_cred: awsCred) {
+        this.aws_cred = aws_cred;
+
+        // Initialize aws-sdk with credentials
+        cwl.config.update({
+            accessKeyId: this.aws_cred.accessKeyId,
+            secretAccessKey: this.aws_cred.secretAccessKey,
+            region: this.aws_cred.region
+        });
+    };
+
+   async sendLogs(params: sendLogsParams, logs: logs) {
+       return new Promise((resolve, reject) => {});
+   };
+
+   async getLogNST(params: getLogNSTParams) {
+       return new Promise((resolve, reject) => {});
+   };
+
+};
